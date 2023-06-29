e00e2dd2a580d20f9f5cdf92d9566840fcc2d594
Vansh Padaliya <vanshp2005@outlook.com>
2023-06-29 13:36:47 +0800

    added time calculation

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const currentDate = new Date ()
+const currentHour = currentDate.getHours(); 
+const currentMinute = currentDate.getMinutes(); 
+const currentSecond = currentDate.getSeconds(); 
+console.log(`Current Time - ${currentHour}:${currentMinute}:${currentSecond}`)
