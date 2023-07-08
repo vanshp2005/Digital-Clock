a2fc4f526586000a790c2cda20db9c5286c3fd94
Vansh Padaliya <vanshp2005@outlook.com>
2023-07-08 22:26:40 +0800

    add digital clock

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const currentDate = new Date ()
-const currentHour = currentDate.getHours(); 
-const currentMinute = currentDate.getMinutes(); 
-const currentSecond = currentDate.getSeconds(); 
-console.log(`Current Time - ${currentHour}:${currentMinute}:${currentSecond}`)
+
+function displaytime() {  
+    const currentDate = new Date ()
+    const currentHour = currentDate.getHours(); 
+    const currentMinute = currentDate.getMinutes(); 
+    const currentSecond = currentDate.getSeconds(); 
+    if (currentHour.toString().length=1) {
+        currentHour = currentHour.replace(/^/, '0')
+    }
+    let time = `${currentHour}:${currentMinute}:${currentSecond}`
+    document.getElementById("timedisplay").innerHTML = time
+}
+setInterval(displaytime, 1000)
