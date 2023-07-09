5d6a2c3982dae1ed7c32c6017ba17e6987baded5
Vansh Padaliya <vanshp2005@outlook.com>
2023-07-09 15:56:53 +0800

    created the final un-edited version of the digital clock display material

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 function displaytime() {  
-    const currentDate = new Date ()
-    const currentHour = currentDate.getHours(); 
-    const currentMinute = currentDate.getMinutes(); 
-    const currentSecond = currentDate.getSeconds(); 
-    if (currentHour.toString().length=1) {
-        currentHour = currentHour.replace(/^/, '0')
-    }
+    let currentDate = new Date ()
+    let currentHour = currentDate.getHours(); 
+    let currentMinute = currentDate.getMinutes(); 
+    let currentSecond = currentDate.getSeconds(); 
+    currentHour = (currentHour < 10) ? "0" + currentHour : currentHour
+    currentMinute = (currentMinute <10) ? "0" + currentMinute : currentMinute; 
+    currentSecond = (currentSecond < 10) ? "0" + currentSecond : currentSecond; 
     let time = `${currentHour}:${currentMinute}:${currentSecond}`
     document.getElementById("timedisplay").innerHTML = time
 }
